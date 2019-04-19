152142881d54323cc8e98553b9258c57b48cc33d
Mitsunari <bennyhwanggggg@users.noreply.github.com>
2019-04-19 20:45:50 +1000

    update code style

--- app.py
+++ app.py
@@ -10,9 +10,9 @@ def main(url='https://whatbot9900backend.herokuapp.com/'):
     start_time = time.time()
     while True:
         time_diff = int(time.time() - start_time)
-        if not time_diff%60:
+        if not time_diff % 60:
             print('Running...'.format(time_diff))
-        if not time_diff%(60 * 29):  # Sends a get request every 29 min
+        if not time_diff % (60*29):  # Sends a get request every 29 min
             result = requests.get(url)
             print(result)
 
